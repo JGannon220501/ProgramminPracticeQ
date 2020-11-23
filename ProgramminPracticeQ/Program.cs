6aa832c1b3a4ed62f7bcb871c058c7afd8172b76
James Gannon <S00196554@mail.itsligo.ie>
2020-11-23 11:54:38 +0000

    Added song class with properties and enum

---
+++ ProgramminPracticeQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProgramminPracticeQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
